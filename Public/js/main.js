8f4f1a8e9d795d0a46b0887a7f5499977b37d45a
tgsegata <tgsegata@gmail.com>
2025-04-14 23:23:07 -0400

    todays trend - json - js

--- Public/js/main.js
+++ Public/js/main.js
@@ -9,16 +9,27 @@ fetch('data/data.json')
 
 
 
-//   cards section
+// bootstrap connection
 async function getRecords() {
+    // connecttion json
     const response = await fetch('./data/data.json');
     const data = await response.json();
     console.log(data);
 
+    // cards section
     document.querySelector('#unresolved-count').textContent = data.unresolved;
     document.querySelector('#overdue-count').textContent = data.overdue;
     document.querySelector('#open-count').textContent = data.open;
     document.querySelector('#onHold-count').textContent = data.onHold;
+
+    // stats column besides graphic
+    document.querySelector('#resolved-stat').textContent = data.resolved;
+    document.querySelector('#received-stat').textContent = data.received;
+    document.querySelector('#first-response-stat').textContent = data.avgFirstResponse;
+    document.querySelector('#avg-response-stat').textContent = data.avgResponse;
+    document.querySelector('#sla-stat').textContent = data.resolutionSLA;
 }
 
+
+//   for running the code
 getRecords();
