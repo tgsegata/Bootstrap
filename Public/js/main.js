48c5c96046a037e67d1212516be9e52f8f1ba5fa
tgsegata <tgsegata@gmail.com>
2025-04-14 22:57:16 -0400

    cards section to json

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,10 +1,24 @@
 // Funciton connection json
 fetch('data/data.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // test output
-    // your loop here
-  })
-  .catch(error => console.error('Error loading JSON:', error));
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // test output
+        // your loop here
+    })
+    .catch(error => console.error('Error loading JSON:', error));
 
-  
+
+
+//   cards section
+async function getRecords() {
+    const response = await fetch('./data/data.json');
+    const data = await response.json();
+    console.log(data);
+
+    document.querySelector('#unresolved-count').textContent = data.unresolved;
+    document.querySelector('#overdue-count').textContent = data.overdue;
+    document.querySelector('#open-count').textContent = data.open;
+    document.querySelector('#onHold-count').textContent = data.onHold;
+}
+
+getRecords();
